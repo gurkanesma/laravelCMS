8b25003704230567473456261a4c1d2d7d1e3659
gurkanesma <gurkanesma8@gmail.com>
2021-09-13 13:00:19 +0300

    submenu modülü oluşturulu

--- app/Models/Models/Models/SubMenus.php
+++ app/Models/Models/Models/SubMenus.php
@@ -7,13 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class SubMenus extends Model
 {
-    protected $table='sub_menu'
+    protected $table='sub_menu';
     public $timestamps='true';
-    protected $fillable[
+    protected $fillable=[
         'menu_id',
         'title',
         'content',
-        'order',
+        'order'
         ];
     use HasFactory;
 }
